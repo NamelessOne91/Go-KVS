f4624603b9fa4e5250743669f796b227f9c42111
paolo invernizzi <paolo.invernizzi@emsibg.com>
2023-03-28 23:55:13 +0200

    Add /{key} GET and PUT endpoints

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/NamelessOne91/Go-KVS/handler"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/{key}", handler.KeyValueGetHandler)
+		r.Put("/{key}", handler.KeyValuePutHandler)
+	})
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
